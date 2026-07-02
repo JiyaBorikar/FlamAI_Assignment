e831c5866618bc0e708fa59416a618df8c35550f
jiya <jiyaborikar16@gmail.com>
2026-07-02 21:57:55 +0530

    Compare generated curve with given data

--- src/result.py
+++ src/result.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 from model import curve
 
-# Read data
 data = pd.read_csv("data/ordered_xy.csv")
 
 # Final values
@@ -12,10 +11,9 @@ theta = 30.0437
 m = 0.029991
 shift = 55.0155
 
-# t values
+
 t = np.linspace(6, 60, len(data))
 
-# Generate curve
 x, y = curve(t, theta, m, shift)
 
 plt.figure(figsize=(7, 7))
